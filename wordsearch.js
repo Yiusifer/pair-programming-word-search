c46231e9b411bd50de9b11e5a9da24297dba67a3
Adam Yiu <adam_yiu@outlook.com>
2022-05-11 23:38:19 +0000

    Pair programming: word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
-module.exports = wordSearch
+const horizontalWordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (const l of horizontalJoin) {
+        //console.log("l = " + l); console.log("word = " + word);
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const transpose2DArray = (letters) => {
+    let result = [];
+
+    for (let i = 0; i < letters[0].length; i++) {
+        result.push([]);
+        for (let j = 0; j < letters.length; j++) {
+            result[i][j] = letters[j][i];
+        }
+    }
+    console.log (result);
+    return result;
+};
+
+const wordSearch = (letters, word) => {
+    let result = horizontalWordSearch(letters, word);
+    if (!result) {
+        let allTransposeLetters = transpose2DArray(letters);
+        result = horizontalWordSearch(allTransposeLetters, word);
+    }
+    return result;
+
+};
+
+module.exports = wordSearch;
